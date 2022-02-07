319dfeb06702d526b7201f6184d1a7e08201a0f7
Ahruda <leogalbierearruda@gmail.com>
2022-02-07 13:30:52 -0300

    project base from start.spring.io

---
+++ src/main/java/com/leonardo/api/RestSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringApiApplication.class, args);
+	}
+
+}
