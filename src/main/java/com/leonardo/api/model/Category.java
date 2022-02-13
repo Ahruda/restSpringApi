202cf3d0815157171ab4781370d01c3940ebf1a9
Ahruda <leogalbierearruda@gmail.com>
2022-02-13 14:57:50 -0300

    id type change

--- src/main/java/com/leonardo/api/model/Category.java
+++ src/main/java/com/leonardo/api/model/Category.java
@@ -9,19 +9,26 @@ import jakarta.persistence.Id;
 public class Category {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String name;
 
-    public Category(int id, String name) {
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public int getId() {
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
