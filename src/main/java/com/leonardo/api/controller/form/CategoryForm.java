3aad68d9dca152584776bc6fcad8f78255f99ad0
Ahruda <leogalbierearruda@gmail.com>
2022-02-15 11:30:13 -0300

    Refactored form class

--- src/main/java/com/leonardo/api/controller/form/CategoryForm.java
+++ src/main/java/com/leonardo/api/controller/form/CategoryForm.java
@@ -9,22 +9,11 @@ public class CategoryForm {
 
     @NotEmpty
     @NotNull(message = "Name cannot be null")
-    @Size(min = 3, max = 20, message = "About Me must be between 10 and 200 characters")
+    @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters")
     private String name;
-
-    public CategoryForm() {
-    }
-
-    public CategoryForm(String name) {
-        this.name = name;
-    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public Category toCategory() {
